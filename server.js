fc9101423f16c42eced181d22dd8f4a88eb0d32c
Juliya Boiko <boikojuliya2707@gmail.com>
2023-02-06 11:45:57 +0200

    add server

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-// const mongoose = require('mongoose');
-// const app = require('./app');
+const mongoose = require('mongoose');
+const app = require('./app');
 
-// const { DB_HOST, PORT = 3000 } = process.env;
+const { DB_HOST, PORT = 3000 } = process.env;
 
-// mongoose.set("strictQuery", false);
+mongoose.set("strictQuery", false);
 
-// mongoose.connect(DB_HOST)
-//   .then(() => {
-//     console.log("Database connection successful");
-//     app.listen(PORT);
-//   })
-//   .catch(error => {
-//     console.log(error.message);
-//     process.exit(1);
-//   });
+mongoose.connect(DB_HOST)
+  .then(() => {
+    console.log("MatoaDB connected successful");
+    app.listen(PORT);
+  })
+  .catch(error => {
+    console.log(error.message);
+    process.exit(1);
+  }); 
